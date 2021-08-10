7c93fb739ca01684d6cd562e680357fc7d421e69
Brandon Bowers <coderbrandonbowers@DESKTOP-4HEFAVI.localdomain>
2021-08-10 16:59:05 -0500

    Bundle created/react component working

---
+++ client/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(){
+      super();
+      this.state = {
+          user: ''
+      }
+  }
+
+    render() {
+        return (<div> 
+            <h1>Why isn't this working</h1>
+        </div>)
+    }
+}
+
+export default App;
